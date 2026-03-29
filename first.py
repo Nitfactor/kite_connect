5877b949a8ec1f132a3663c880ec51939d87c169
Akshat Singh <nitfactor@gmail.com>
2026-03-29 14:00:05 +0530

    main

--- first.py
+++ first.py
@@ -1,1 +1,8 @@
-# This is Rabola.com
+# This is Rabola.com 
+import yfinance as yf
+import pandas as pd
+
+ticker = yf.Ticker("TCS.NS")
+
+df = ticker.history(period="1mo")
+print(df[["Open", "Close", "Volume"]])
