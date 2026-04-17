347f7e2aab56288834a850d05b88e1acfcb21e6c
Akshat Singh <nitfactor@gmail.com>
2026-04-18 01:10:37 +0530

    main

--- backend/app/kite_core.py
+++ backend/app/kite_core.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from routes.margins import show_margins
 
 load_dotenv()
-
 api_key = os.getenv("KITE_API_KEY")
 api_secret = os.getenv("KITE_API_SECRET")
 kite = KiteConnect(api_key=api_key)
